f5843a650beae544e7dc33221b8b18a67a7b0391
dawillygene <eliamariki2000@gmail.com>
2026-06-11 02:06:51 +0300

    Export backend messaging helpers

---
+++ bodaboda-app/backend/messaging/__init__.py
@@ -0,0 +1,19 @@
+from .publisher import (
+    build_ride_request_payload,
+    build_ride_status_payload,
+    get_publisher,
+    override_publisher,
+    publish_ride_request,
+    publish_ride_status,
+    reset_publisher,
+)
+
+__all__ = [
+    "build_ride_request_payload",
+    "build_ride_status_payload",
+    "get_publisher",
+    "override_publisher",
+    "publish_ride_request",
+    "publish_ride_status",
+    "reset_publisher",
+]
